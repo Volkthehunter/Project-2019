7a5098d6964dcbd97c0e6f0ee902f96c9e5c3e9d
Volkthehunter <46421477+Volkthehunter@users.noreply.github.com>
2019-12-19 16:31:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+import module1
+import math
+video_capture = cv2.VideoCapture(0)
+# Run the infinite loop
+while True:
+    # Read each frame
+    ret, frame = video_capture.read()
+    #print(frame)
+    # Convert frame to grey because cascading only works with greyscale image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Call the detect function with grey image and colored frame
+    canvas = module1.detect(gray, frame)
+    # Show the image in the screen
+    cv2.imshow("Video", canvas)
+    # Put the condition which triggers the end of program
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
